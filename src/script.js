dabd765e8c211b231ff68bf562e344a477ddcdcf
Radek-01 <rlasiecki@gmail.com>
2023-04-22 08:07:08 +0200

    newv

--- src/script.js
+++ src/script.js
@@ -617,6 +617,6 @@ async function getGitHubFile(owner, repo, path) {
   return data;
 }
 
-getGitHubFile("Radek-01", "quizApp", "quizSet1.json").then((data) => {
-  console.log("new fetch", data);
-});
+// getGitHubFile("Radek-01", "quizApp", "quizSet1.json").then((data) => {
+//   console.log("new fetch", data);
+// });
