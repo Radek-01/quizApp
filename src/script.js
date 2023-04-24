22c5156ef90d4383f3d3f053ae99cf66da8adc60
Radek-01 <rlasiecki@gmail.com>
2023-04-24 09:38:55 +0200

    newv

--- src/script.js
+++ src/script.js
@@ -3,8 +3,6 @@
 //temporary
 if (window.location.hostname === "radek-01.github.io") {
   console.log("github");
-  const { Octokit } = require("@octokit/core");
-  console.log(Octokit);
 } else {
   console.log("local server");
 }
